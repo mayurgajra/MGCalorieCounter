eea2da4708828d9697e152074d6d5cd04084d1b5
Mayur Gajra <mayurgajra5@gmail.com>
2022-01-23 20:05:39 +0530

    - Initial commit

---
+++ app/src/main/java/com/mgcaloriecounter/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.mgcaloriecounter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.mgcaloriecounter.ui.theme.MGCalorieCounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MGCalorieCounterTheme {
+
+            }
+        }
+    }
+}
