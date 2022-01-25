b058ddb7ab6ea8701e676afb77970138c95b395c
Mayur Gajra <mayurgajra5@gmail.com>
2022-01-25 08:56:08 +0530

    - Welcome screen implemented for onboarding

---
+++ onboarding/onboarding_presentation/src/main/java/com/mayurg/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -0,0 +1,36 @@
+package com.mayurg.onboarding_presentation.welcome
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.mayurg.core.R
+import com.mayurg.core_ui.LocalSpacing
+import com.mayurg.onboarding_presentation.components.ActionButton
+
+@Composable
+fun WelcomeScreen() {
+    val spacing = LocalSpacing.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.welcome_text),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h1
+        )
+        Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        ActionButton(
+            text = stringResource(id = R.string.next),
+            onClick = { /*TODO*/ },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+    }
+}
