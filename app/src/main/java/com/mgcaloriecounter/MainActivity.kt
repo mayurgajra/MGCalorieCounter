3c26161b036b4d446f17a7b52f07a40c02e5443b
Mayur Gajra <mayurgajra5@gmail.com>
2022-01-29 14:49:56 +0530

    - Gender selection implemented

--- app/src/main/java/com/mgcaloriecounter/MainActivity.kt
+++ app/src/main/java/com/mgcaloriecounter/MainActivity.kt
@@ -3,19 +3,17 @@ package com.mgcaloriecounter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.mayurg.core.navigation.Route
+import com.mayurg.onboarding_presentation.gender.GenderScreen
 import com.mayurg.onboarding_presentation.welcome.WelcomeScreen
 import com.mgcaloriecounter.navigation.navigate
 import com.mgcaloriecounter.ui.theme.MGCalorieCounterTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +23,13 @@ class MainActivity : ComponentActivity() {
                 NavHost(
                     navController = navController,
                     startDestination = Route.WELCOME
-                ){
-                    composable(route = Route.WELCOME){
+                ) {
+                    composable(route = Route.WELCOME) {
                         WelcomeScreen(onNavigate = navController::navigate)
+                    }
+
+                    composable(Route.GENDER) {
+                        GenderScreen(onNavigate = navController::navigate)
                     }
                 }
             }
