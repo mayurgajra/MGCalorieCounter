96728d764c802b0cd82fd7077610d3db8de2fb93
Mayur Gajra <mayurgajra5@gmail.com>
2022-02-02 09:14:58 +0530

    - Repository setup done for tracker

--- tracker/tracker_data/src/main/java/com/mayurg/tracker_data/di/TrackerDataModule.kt
+++ tracker/tracker_data/src/main/java/com/mayurg/tracker_data/di/TrackerDataModule.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.room.Room
 import com.mayurg.tracker_data.local.TrackerDatabase
 import com.mayurg.tracker_data.remote.OpenFoodApi
+import com.mayurg.tracker_data.repository.TrackerRepositoryImpl
+import com.mayurg.tracker_domain.repository.TrackerRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -48,5 +50,17 @@ object TrackerDataModule {
             TrackerDatabase::class.java,
             "tracker_db"
         ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTrackerRepository(
+        api: OpenFoodApi,
+        db: TrackerDatabase
+    ): TrackerRepository {
+        return TrackerRepositoryImpl(
+            dao = db.dao,
+            api = api
+        )
     }
 }
