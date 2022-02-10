9beb2a01bfe5d405dcdf607f97e969134b0a85e3
Mayur Gajra <mayurgajra5@gmail.com>
2022-02-10 09:13:08 +0530

    - Search viewmodel & state management done

---
+++ tracker/tracker_presentation/src/main/java/com/mayurg/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.mayurg.tracker_presentation.search
+
+import com.mayurg.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
