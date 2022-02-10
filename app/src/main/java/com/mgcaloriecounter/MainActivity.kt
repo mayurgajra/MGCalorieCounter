f263d36d78d2cd0ca2b999b0816cc633e2f4e717
Mayur Gajra <mayurgajra5@gmail.com>
2022-02-10 20:51:46 +0530

    - Search text field UI created

--- app/src/main/java/com/mgcaloriecounter/MainActivity.kt
+++ app/src/main/java/com/mgcaloriecounter/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.mayurg.core.navigation.Route
 import com.mayurg.onboarding_presentation.activity.ActivityScreen
 import com.mayurg.onboarding_presentation.age.AgeScreen
@@ -19,6 +21,7 @@ import com.mayurg.onboarding_presentation.height.HeightScreen
 import com.mayurg.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.mayurg.onboarding_presentation.weight.WeightScreen
 import com.mayurg.onboarding_presentation.welcome.WelcomeScreen
+import com.mayurg.tracker_presentation.search.SearchScreen
 import com.mayurg.tracker_presentation.tracker_overview.TrackerOverViewScreen
 import com.mgcaloriecounter.navigation.navigate
 import com.mgcaloriecounter.ui.theme.MGCalorieCounterTheme
@@ -86,6 +89,38 @@ class MainActivity : ComponentActivity() {
 
                         composable(Route.TRACKER_OVERVIEW) {
                             TrackerOverViewScreen(onNavigate = navController::navigate)
+                        }
+
+                        composable(
+                            route = Route.SEARCH + "/{mealName}/{dayOfMonth}/{month}/{year}",
+                            arguments = listOf(
+                                navArgument("mealName") {
+                                    type = NavType.StringType
+                                },
+                                navArgument("dayOfMonth") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("month") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("year") {
+                                    type = NavType.IntType
+                                }
+                            )
+                        ) {
+                            val mealName = it.arguments?.getString("mealName")!!
+                            val dayOfMonth = it.arguments?.getInt("dayOfMonth")!!
+                            val month = it.arguments?.getInt("month")!!
+                            val year = it.arguments?.getInt("year")!!
+                            SearchScreen(
+                                scaffoldState = scaffoldState,
+                                mealName = mealName,
+                                dayOfMonth = dayOfMonth,
+                                month = month,
+                                year = year,
+                                onNavigateUp = {
+                                    navController.navigateUp()
+                                })
                         }
                     }
                 }
--- tracker/tracker_presentation/src/main/java/com/mayurg/tracker_presentation/tracker_overview/TrackerOverViewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/mayurg/tracker_presentation/tracker_overview/TrackerOverViewScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -22,6 +23,17 @@ fun TrackerOverViewScreen(
     val spacing = LocalSpacing.current
     val state = viewModel.state
     val context = LocalContext.current
+
+    LaunchedEffect(key1 = context){
+        viewModel.uiEvent.collect{ event ->
+            when(event){
+                is UiEvent.Navigate -> {
+                    onNavigate(event)
+                }
+                else -> Unit
+            }
+        }
+    }
 
     LazyColumn(
         modifier = Modifier
